e4254b5f8e55d38ffc3891b96ca85a79fd097834
colinmfoster4723 <colin.foster4723@gmail.com>
2023-10-08 21:52:59 -0500

    Added local storage set for light/dark mode

--- components/LightModeToggle.tsx
+++ components/LightModeToggle.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function LightModeToggle({
   lightMode,
@@ -8,7 +8,10 @@ export function LightModeToggle({
   return (
     <button
       className={`flex gap-2 text-4xl px-4 py-2 bg-[#1d1d1d50] rounded-lg ${className}`}
-      onClick={() => setLightMode(!lightMode)}
+      onClick={() => {
+        setLightMode(!lightMode);
+        localStorage.setItem("lightMode", JSON.stringify(!lightMode));
+      }}
     >
       <p>{"◎"}</p>|<p>{"◉"}</p>
       <div
