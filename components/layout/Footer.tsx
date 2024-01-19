231214c483fd934aef55ef82ba608dae1b6227e6
colinmfoster4723 <colin.foster4723@gmail.com>
2024-01-19 16:51:52 -0600

    Adjusted footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <div className="my-12 p-4 flex gap-[40%] justify-center">
+    <div className="my-12 p-4 flex gap-12 md:gap-[5%] justify-center items-center">
       <Link
         className="hover:scale-[1.1] transition-all duration-300 opacity-80"
         href="https://github.com/colinmfoster4723"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <Image src="/github-mark.png" width={82} height={82} alt="" />
+        <Image src="/github-mark.png" width={60} height={60} alt="" />
       </Link>
       <Link
         className="hover:scale-[1.1] transition-all duration-300 opacity-80"
@@ -18,7 +18,7 @@ export function Footer() {
         target="_blank"
         rel="noopener noreferrer"
       >
-        <Image src="/x-logo.png" width={68} height={68} alt="" />
+        <Image src="/x-logo.png" width={50} height={50} alt="" />
       </Link>
     </div>
   );
