f0a9b7d11dd5d14d2835b991575a52431a567c15
colinmfoster4723 <colin.foster4723@gmail.com>
2024-05-22 11:18:17 -0500

    Updated github link

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
     <div className="my-12 p-4 flex gap-12 md:gap-[5%] justify-center items-center">
       <Link
         className="hover:scale-[1.1] transition-all duration-300 opacity-80"
-        href="https://github.com/colinmfoster4723"
+        href="https://github.com/namedotget"
         target="_blank"
         rel="noopener noreferrer"
       >
